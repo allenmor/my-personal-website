d0c49364ecdf0649d4b2c89d3b3d4083353390ed
Allen Mordukhaev <allenmor93@gmail.com>
2022-06-25 19:29:03 -0400

    update

--- scriptBlackjack.js
+++ scriptBlackjack.js
@@ -35,6 +35,19 @@ function deckBtn() {
     amountOfCards = decks * 52;
     console.log(amountOfCards);
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
+    twosLeft = document.getElementById('twosLeft').innerHTML = amountOfCards / 13;
+    threesLeft = document.getElementById('threesLeft').innerHTML = amountOfCards / 13;
+    foursLeft = document.getElementById('foursLeft').innerHTML = amountOfCards / 13;
+    fivesLeft = document.getElementById('fivesLeft').innerHTML = amountOfCards / 13;
+    sixsLeft = document.getElementById('sixsLeft').innerHTML = amountOfCards / 13;
+    sevensLeft = document.getElementById('sevensLeft').innerHTML = amountOfCards / 13;
+    eightsLeft = document.getElementById('eightsLeft').innerHTML = amountOfCards / 13;
+    ninesLeft = document.getElementById('ninesLeft').innerHTML = amountOfCards / 13;
+    tensLeft = document.getElementById('tensLeft').innerHTML = amountOfCards / 13;
+    jacksLeft = document.getElementById('jacksLeft').innerHTML = amountOfCards / 13;
+    queensLeft = document.getElementById('queensLeft').innerHTML = amountOfCards / 13;
+    kingsLeft = document.getElementById('kingsLeft').innerHTML = amountOfCards / 13;
+    acesLeft = document.getElementById('acesLeft').innerHTML = amountOfCards / 13;
 }
 
 
@@ -49,6 +62,8 @@ function btn2 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    twosLeft -= 1;
+    document.getElementById('twosLeft').innerHTML = twosLeft;
 
 }
 function btn3 () {
@@ -60,6 +75,8 @@ function btn3 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    threesLeft -= 1;
+    document.getElementById('threesLeft').innerHTML = threesLeft;
 }
 function btn4 () {
     runningCount += 1;
@@ -70,6 +87,8 @@ function btn4 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    foursLeft -= 1;
+    document.getElementById('foursLeft').innerHTML = foursLeft;
 }
 function btn5 () {
     runningCount += 1;
@@ -80,6 +99,8 @@ function btn5 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    fivesLeft -= 1;
+    document.getElementById('fivesLeft').innerHTML = fivesLeft;
 }
 function btn6 () {
     runningCount += 1;
@@ -90,24 +111,32 @@ function btn6 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    sixsLeft -= 1;
+    document.getElementById('sixsLeft').innerHTML = sixsLeft;
 }
 function btn7 () {
     amountOfCards -= 1;
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    sevensLeft -= 1;
+    document.getElementById('sevensLeft').innerHTML = sevensLeft;
 }
 function btn8 () {
     amountOfCards -= 1;
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    eightsLeft -= 1;
+    document.getElementById('eightsLeft').innerHTML = eightsLeft;
 }
 function btn9 () {
     amountOfCards -= 1;
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    ninesLeft -= 1;
+    document.getElementById('ninesLeft').innerHTML = ninesLeft;
 }
 function btn10 () {
     runningCount -= 1;
@@ -118,6 +147,8 @@ function btn10 () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    tensLeft -= 1;
+    document.getElementById('tensLeft').innerHTML = tensLeft;
 }
 function btnJ () {
     runningCount -= 1;
@@ -128,6 +159,8 @@ function btnJ () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    jacksLeft -= 1;
+    document.getElementById('jacksLeft').innerHTML = jacksLeft;
 }
 function btnQ () {
     runningCount -= 1;
@@ -138,6 +171,8 @@ function btnQ () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    queensLeft -= 1;
+    document.getElementById('queensLeft').innerHTML = queensLeft;
 }
 function btnK () {
     runningCount -= 1;
@@ -148,6 +183,8 @@ function btnK () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    kingsLeft -= 1;
+    document.getElementById('kingsLeft').innerHTML = kingsLeft;
 }
 function btnA () {
     runningCount -= 1;
@@ -158,6 +195,8 @@ function btnA () {
     document.getElementById('amountOfCards').innerHTML = amountOfCards;
     trueCount = runningCount / (amountOfCards / 52);
     document.getElementById('trueCount').innerHTML = trueCount.toFixed(2);
+    acesLeft -= 1;
+    document.getElementById('acesLeft').innerHTML = acesLeft;
 }
 
 
